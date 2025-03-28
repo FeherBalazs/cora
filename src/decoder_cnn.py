fcfc5b8328f0c1e2f494eb013e314816fd31e93e
Balazs Feher <feher.balazs.feher@gmail.com>
2025-03-28 18:36:23 +0000

    Update dataset path in decoder_cnn.py for CIFAR-10 configuration
    - Changed the root path for dataset loading to point to the CIFAR-10 example directory, ensuring correct data access for model training and evaluation.
    - Added a comment to clarify the dataset selection process for improved usability.

--- src/decoder_cnn.py
+++ src/decoder_cnn.py
@@ -395,7 +395,8 @@ def visualize_reconstruction(model, optim_h, dataloader, T_values=[24], use_corr
 if __name__ == '__main__':
     # Added dataset selection and configuration
     dataset_name = "cifar10"  # Change to "cifar10" or "imagenet" "fashionmnist" as needed
-    root_path = "../datasets/"  # Adjust to your dataset root path
+    root_path = "../examples/datacifar10/"
+    
     batch_size = 100
     nm_epochs = 20
     target_class = None
